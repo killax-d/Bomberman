46faeb65e20aea4261c6e968848d739e906fbf07
Williams Gest <contact@williams-gest.com>
2019-04-28 12:57:40 -0400

    Setup maven projects

---
+++ Client/Bomberman/src/main/java/fr/bomberman/client/App.java
@@ -0,0 +1,9 @@
+package fr.bomberman.client;
+
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Server/server/src/main/java/fr/bomberman/server/App.java
@@ -0,0 +1,9 @@
+package fr.bomberman.server;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+	}
+	
+}
